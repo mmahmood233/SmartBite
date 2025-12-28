1ef0a4dc38e3a3ba2fddb81e95a9b26c549ea5da
mmahmood233 <m7mdm233@gmail.com>
2025-12-28 22:58:22 +0300

    cleanup

--- check_order_items_schema.sql
+++
@@ -1,9 +0,0 @@
--- Check order_items table schema
-
-SELECT 
-    column_name,
-    data_type,
-    is_nullable
-FROM information_schema.columns
-WHERE table_name = 'order_items'
-ORDER BY ordinal_position;
